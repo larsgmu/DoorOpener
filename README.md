e01c2ae8faf662b1fdb54b3cef1fcb0881f8f905
larsgmu <31850889+larsgmu@users.noreply.github.com>
2020-04-23 23:18:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DoorOpener
+
+This is a home automation project with the aim to use my phone to let people in my apartment. As a proof of concept i use Blynk library running on an arduino which will enable a relay that activates the door opener in the apartment with a push of a button in the Blynk app.
+
+<img src="img/sh1.PNG" alt="screenshot1"
+	title="Blynk app" width="300" height="600" /> <img src="img/sh2.PNG" alt="screenshot2"
+	title="Blynk app" width="300" height="600" />
---
+++ main.ino
@@ -0,0 +1,65 @@
+
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "AUTH TOKEN";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "INTERNET SSID";
+char pass[] = "INTERNET PASSWORD";
+
+// Hardware Serial on Mega, Leonardo, Micro...
+//#define EspSerial Serial1
+
+// or Software Serial on Uno, Nano...
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, millis() / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(auth, wifi, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, wifi, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
